6e61af5a6379aa3a2daa8cbbeae193865c633ede
Julia Piaścik <98356859+liapia99@users.noreply.github.com>
2024-11-30 18:03:57 -0500

    Update TCPClient.py

--- TCPClient.py
+++ TCPClient.py
@@ -11,18 +11,18 @@ clientSocket.connect((serverName, serverPort))
 
 
 
-
 plane_class = input("Enter class (B for Business, E for Economy): ").strip()
 clientSocket.send(plane_class.encode("utf-8"))
 
 seats = input("Enter number of passengers: ").strip()
-clientSocket.send(seats.encode("utf-8"))  
+clientSocket.send(seats.encode("utf-8"))
 
 carryon = input("Enter number of carry-ons: ").strip()
-clientSocket.send(carryon.encode("utf-8")) 
+clientSocket.send(carryon.encode("utf-8"))
 
 suitcases = input("Enter number of luggages: ").strip()
-clientSocket.send(suitcases.encode("utf-8")) 
+clientSocket.send(suitcases.encode("utf-8"))
+
 
 response = clientSocket.recv(1024).decode("utf-8")
 print("Receipt:", response)
