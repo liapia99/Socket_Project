28493a6888b8649fac5545bf515cd9081af26ffc
Julia Piaścik <98356859+liapia99@users.noreply.github.com>
2024-12-01 20:22:25 -0500

    Update TCPServer.py

--- TCPServer.py
+++ TCPServer.py
@@ -1,7 +1,7 @@
 from socket import *
 
 # has to be the same port as the client!
-serverPort = 12009
+serverPort = 12011
 
 # create TCP welcoming socket
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -11,8 +11,8 @@ serverSocket.bind(("", serverPort))
 serverSocket.listen(1)
 
 
-class_B = 30  # Limit of Business class seats
-class_E = 120  # Limit of Economy class seats
+class_B = 30  # limit of Business class seats
+class_E = 120  # limit of Economy class seats
 
 carry_on = 0
 luggage = 0
@@ -27,15 +27,14 @@ print("The Socket Project running over TCP is ready to receive...")
 
 while 1:
     # server waits for incoming requests; new socket created on return
-    connectionSocket, addr = serverSocket.accept()
+    connectionSocket, addr = serverSocket.accept()  # Correct with parentheses
      
     # read a sentence of bytes from socket sent by the client
-    connectionSocket, addr = serverSocket.accept# Receive and process data from client
     
-    plane_class = connectionSocket.recv(1024).decode("utf-8").strip()
-    seats = connectionSocket.recv(1024).decode("utf-8").strip()
-    carryon = connectionSocket.recv(1024).decode("utf-8").strip()
-    suitcases = connectionSocket.recv(1024).decode("utf-8").strip()
+    plane_class = str(connectionSocket.recv(1025).strip(), "utf-8")
+    seats = str(connectionSocket.recv(1025).strip(), "utf-8")
+    carryon = str(connectionSocket.recv(1025).strip(), "utf-8")
+    suitcases = str(connectionSocket.recv(1025).strip(), "utf-8")
 
     seat_num = int(seats)
     carry_on = int(carryon)
@@ -50,16 +49,17 @@ while 1:
             response = "No seats left in Business class."
         # if there are enough seats (greater than or equal to)
         elif class_B >= seat_num:
-            class_B -= seat_num #if there are enough seats, subtract the taken seats from the available seating
+             #if there are enough seats, subtract the taken seats from the available seating
             # check the carry-ons and luggages ( if they meet the requirements )
-            if carry_on == seat_num and luggage <= (seat_num * 2):
+            if carry_on <= seat_num and luggage <= (seat_num * 2):
+                class_B -= seat_num
                 # calculate cost of seat only 
                 cost = seat_num * 200
                 # calculate cost of luggage and carry-on only 
                 extracost = (carry_on * 20) + (luggage * 50)
 
                 totalcost = cost + extracost
-                response = (f"Great, You got {seat_num} for ${totalcost}."
+                response = (f"Great, You got {seat_num} for ${totalcost}. "
                             f"Remaining seats: Economy {class_E} Business {class_B}")
             else:
                 response = "Baggage policy violated: 1 carry-on per person, up to 2 luggages per person."
@@ -72,15 +72,15 @@ while 1:
             response = "No seats left in Economy class."
             # if there are enough seats (greater than or equal to)
         elif class_E >= seat_num:
-            class_E -= seat_num
-            if carry_on == seat_num and luggage <= (seat_num * 2):
+            if carry_on <= seat_num and luggage <= (seat_num * 2):
+                class_E -= seat_num
                 # calculate cost of seat only 
                 cost = seat_num * 80
                 # calculate cost of luggage and carry-on only 
                 extracost = (carry_on * 20) + (luggage * 50)
 
                 totalcost = cost + extracost
-                response = (f"Great, You got {seat_num} for ${totalcost}."
+                response = (f"Great, You got {seat_num} for ${totalcost}. "
                             f"Remaining seats: Economy {class_E} Business {class_B}")
             else:
                 response = "Baggage policy violated: 1 carry-on per person, up to 2 luggages per person."
@@ -92,7 +92,7 @@ while 1:
 
 
     # output to console the sentence sent back to the client 
-    connectionSocket.send(response.encode("utf-8"))
+    connectionSocket.send(bytes(response, "utf-8"))
 
     # close the TCP connection; the welcoming socket continues
     connectionSocket.close()
